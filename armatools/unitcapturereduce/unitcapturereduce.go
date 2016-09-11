f049b45a20e4691b64ec8a1301e4aa20b92f0db3
Euan J Hunter <euanhunter117@gmail.com>
2016-09-11 15:39:31 +0100

    Changed from using custom linked-list to container/list and improved error checking

--- armatools/unitcapturereduce/unitcapturereduce.go
+++ armatools/unitcapturereduce/unitcapturereduce.go
@@ -2,163 +2,202 @@ package unitcapturereduce
 
 import (
 	"bytes"
+	"container/list"
 	"encoding/json"
 	"fmt"
-	"math"
 )
 
 // ReduceUnitCapture reduces BIS_fnc_UnitCapture in a lossy manner using an error threshold
-func ReduceUnitCapture(rawCaptureData string, errorPercentThreshold float64) (string, int, int, error) {
+func ReduceUnitCapture(rawCaptureData string, probabilityThreshold float64) (string, int, int, error) {
 	captureData, err := parseCaptureData(rawCaptureData)
 	if err != nil {
 		return "", 0, 0, err
 	}
 
 	before := captureData.numberOfFrames()
-
 	if before < 3 {
 		return "", 0, 0, fmt.Errorf("There must be atleast 3 capture frames")
 	}
 
-	captureData.reduce(errorPercentThreshold)
+	captureData.reduce(probabilityThreshold)
 
 	after := captureData.numberOfFrames()
 
-	return captureData.String(), before, after, nil
+	return captureData.SQFString(), before, after, nil
 }
 
-type captureFrame struct {
-	Time float64
-
-	Position  vec3
-	Direction vec3
-	Up        vec3
-	Velocity  vec3
-
-	Before *captureFrame
-	Next   *captureFrame
-}
-
-func parseCaptureData(rawCaptureData string) (*captureFrame, error) {
+func parseCaptureData(rawCaptureData string) (*captureKeyFrames, error) {
 	var unsafeCaptureData [][]interface{}
 	err := json.Unmarshal([]byte(rawCaptureData), &unsafeCaptureData)
 	if err != nil {
 		return nil, err
 	}
 
-	var next *captureFrame
-	first := (*captureFrame)(nil)
-	before := (*captureFrame)(nil)
+	data := list.New()
 
-	for i, unsafeFrame := range unsafeCaptureData {
-		if i == 0 {
-			first = &captureFrame{}
-			next = first
+	for i, unsafeKeyFrame := range unsafeCaptureData {
+		time, timeOK := unsafeKeyFrame[0].(float64)
+
+		unsafePosition, unsafePositionOK := unsafeKeyFrame[1].([]interface{})
+		unsafeDirection, unsafeDirectionOK := unsafeKeyFrame[2].([]interface{})
+		unsafeUp, unsafeUpOK := unsafeKeyFrame[3].([]interface{})
+		unsafeVelocity, unsafeVelocityOK := unsafeKeyFrame[4].([]interface{})
+
+		position := unsafeSliceToVec3(unsafePosition)
+		direction := unsafeSliceToVec3(unsafeDirection)
+		up := unsafeSliceToVec3(unsafeUp)
+		velocity := unsafeSliceToVec3(unsafeVelocity)
+
+		if !timeOK ||
+			!unsafePositionOK || !unsafeDirectionOK || !unsafeUpOK || !unsafeVelocityOK ||
+			position == nil || direction == nil || up == nil || velocity == nil {
+
+			return nil, fmt.Errorf("Invalid Unit Capture output format")
 		}
 
-		next.Time = unsafeFrame[0].(float64)
-		next.Position = unsafeSliceToVec3(unsafeFrame[1].([]interface{}))
-		next.Direction = unsafeSliceToVec3(unsafeFrame[2].([]interface{}))
-		next.Up = unsafeSliceToVec3(unsafeFrame[3].([]interface{}))
-		next.Velocity = unsafeSliceToVec3(unsafeFrame[4].([]interface{}))
-		next.Before = before
-
-		before = next
-		next = &captureFrame{}
-
-		if i != len(unsafeCaptureData)-1 {
-			before.Next = next
-		} else {
-			before.Next = nil
+		keyframe := &captureKeyFrame{
+			OriginalFrameNumber: newFinalInt(i),
+			Time:                newFinalFloat64(time),
+			Position:            *position,
+			Direction:           *direction,
+			Up:                  *up,
+			Velocity:            *velocity,
 		}
+
+		data.PushBack(keyframe)
 	}
 
-	return first, nil
+	return (*captureKeyFrames)(data), nil
 }
 
-func (captureData *captureFrame) reduce(errorPercentThreshold float64) {
-	start := captureData
-	consider := start.Next
-	end := consider.Next
+func unsafeSliceToVec3(slice []interface{}) *vec3 {
+	if len(slice) != 3 {
+		return nil
+	}
+
+	a, aOK := slice[0].(float64)
+	b, bOK := slice[1].(float64)
+	c, cOK := slice[2].(float64)
+
+	if !aOK || !bOK || !cOK {
+		return nil
+	}
+
+	return &vec3{
+		A: newFinalFloat64(a),
+		B: newFinalFloat64(b),
+		C: newFinalFloat64(c),
+	}
+}
+
+type captureKeyFrames list.List
+
+func (ckf *captureKeyFrames) numberOfFrames() int {
+	return (*list.List)(ckf).Len()
+}
+
+func (ckf *captureKeyFrames) reduce(probabilityThreshold float64) {
+	count := 0
+
+	startElm := (*list.List)(ckf).Front()
+	considerElm := startElm.Next()
+	endElm := considerElm.Next()
+
+	start := startElm.Value.(*captureKeyFrame)
+	consider := considerElm.Value.(*captureKeyFrame)
+	end := endElm.Value.(*captureKeyFrame)
 
 	for {
-		lerped := lerpCaptureFrame(start, end, consider.Time)
-
-		if consider.maxPercentDifference(lerped) < errorPercentThreshold {
-			start.Next = end
-			end.Before = start
+		if start.OriginalFrameNumber.Value() != count {
+			panic("A frame has been skipped")
 		}
 
-		if end.Next == nil {
+		newFrame := start.lerp(end, consider.Time.Value())
+
+		if vectorisedNormalDistributionPDF(newFrame.toSlice(), consider.toSlice(), 1) > probabilityThreshold {
+			(*list.List)(ckf).Remove(considerElm)
+		}
+
+		if endElm.Next() == nil {
 			break
 		}
 
-		start = start.Next
-		consider = start.Next
-		end = consider.Next
+		startElm = startElm.Next()
+		considerElm = startElm.Next()
+		endElm = considerElm.Next()
 
-		if end == nil {
+		if endElm == nil {
 			break
 		}
+
+		start = startElm.Value.(*captureKeyFrame)
+		consider = considerElm.Value.(*captureKeyFrame)
+		end = endElm.Value.(*captureKeyFrame)
+
+		count++
 	}
 }
 
-func (captureData *captureFrame) String() string {
+func (ckf *captureKeyFrames) SQFString() string {
 	buf := &bytes.Buffer{}
-	current := captureData
 
 	fmt.Fprint(buf, "[")
-	for current != nil {
-		fmt.Fprintf(buf, "[%v,%s,%s,%s,%s]", current.Time, current.Position.String(), current.Direction.String(), current.Up.String(), current.Velocity.String())
+	for elm := (*list.List)(ckf).Front(); elm != nil; elm = elm.Next() {
+		fmt.Fprint(buf, elm.Value.(*captureKeyFrame).SQFString())
 
-		if current.Next != nil {
+		if elm.Next() != nil {
 			fmt.Fprint(buf, ",")
 		}
-
-		current = current.Next
 	}
 	fmt.Fprint(buf, "]")
 
 	return buf.String()
 }
 
-func (captureData *captureFrame) numberOfFrames() int {
-	count := 0
-	current := captureData
-
-	for current != nil {
-		count++
-		current = current.Next
-	}
-
-	return count
+type captureKeyFrame struct {
+	OriginalFrameNumber finalInt
+	Time                finalFloat64
+	Position            vec3
+	Direction           vec3
+	Up                  vec3
+	Velocity            vec3
 }
 
-func lerpCaptureFrame(start *captureFrame, end *captureFrame, time float64) *captureFrame {
-	t := (time - start.Time) / end.Time
+func (ckf *captureKeyFrame) lerp(end *captureKeyFrame, time float64) *captureKeyFrame {
+	t := (time - ckf.Time.Value()) / end.Time.Value()
 
-	return &captureFrame{
-		Time:      time,
-		Position:  start.Position.lerp(end.Position, t),
-		Direction: start.Direction.lerp(end.Direction, t),
-		Up:        start.Up.lerp(end.Up, t),
-		Velocity:  start.Velocity.lerp(end.Velocity, t),
+	return &captureKeyFrame{
+		OriginalFrameNumber: newFinalInt(-1),
+		Time:                newFinalFloat64(time),
+		Position:            ckf.Position.lerp(end.Position, t),
+		Direction:           ckf.Direction.lerp(end.Direction, t),
+		Up:                  ckf.Up.lerp(end.Up, t),
+		Velocity:            ckf.Velocity.lerp(end.Velocity, t),
 	}
 }
 
-func (captureData *captureFrame) maxPercentDifference(other *captureFrame) float64 {
-	positionDifference := captureData.Position.maxPercentDifference(other.Position)
-	directionDifference := captureData.Direction.maxPercentDifference(other.Direction)
-	upDifference := captureData.Up.maxPercentDifference(other.Up)
-	velocityDifference := captureData.Velocity.maxPercentDifference(other.Velocity)
+func (ckf *captureKeyFrame) toSlice() []float64 {
+	slice := make([]float64, 12)
 
-	return math.Max(positionDifference, math.Max(directionDifference, math.Max(upDifference, velocityDifference)))
+	slice[0] = ckf.Position.A.Value()
+	slice[1] = ckf.Position.B.Value()
+	slice[2] = ckf.Position.C.Value()
+
+	slice[3] = ckf.Direction.A.Value()
+	slice[4] = ckf.Direction.B.Value()
+	slice[5] = ckf.Direction.C.Value()
+
+	slice[6] = ckf.Up.A.Value()
+	slice[7] = ckf.Up.B.Value()
+	slice[8] = ckf.Up.C.Value()
+
+	slice[9] = ckf.Velocity.A.Value()
+	slice[10] = ckf.Velocity.B.Value()
+	slice[11] = ckf.Velocity.C.Value()
+
+	return slice
 }
 
-func unsafeSliceToVec3(slice []interface{}) vec3 {
-	return vec3{
-		A: slice[0].(float64),
-		B: slice[1].(float64),
-		C: slice[2].(float64),
-	}
+func (ckf *captureKeyFrame) SQFString() string {
+	return fmt.Sprintf("[%v,%s,%s,%s,%s]", ckf.Time.Value(), ckf.Position.SQFString(), ckf.Direction.SQFString(), ckf.Up.SQFString(), ckf.Velocity.SQFString())
 }
